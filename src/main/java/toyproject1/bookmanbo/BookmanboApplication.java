13ed77f0cb3d8cb71ae8685b05b557aaf9503804
nonoro <kgu1022@naver.com>
2023-01-10 22:32:48 +0900

    스프링부트 프로젝트 기본 생성 클래스

---
+++ src/main/java/toyproject1/bookmanbo/BookmanboApplication.java
@@ -0,0 +1,13 @@
+package toyproject1.bookmanbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmanboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toyproject1/bookmanbo/BookmanboApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject1.bookmanbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
