d722a6bef4c983bff761836e6cab02183beda2a1
nonoro <kgu1022@naver.com>
2023-01-13 01:53:18 +0900

    테스트 코드에 적용할 JpaAuditing config 설정 추가

---
+++ src/test/java/toyproject1/bookmanbo/config/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package toyproject1.bookmanbo.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@TestConfiguration
+public class JpaConfiguration {
+}
